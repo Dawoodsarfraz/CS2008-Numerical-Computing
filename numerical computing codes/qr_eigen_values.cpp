928a0939e4fccfc3ad8a5fb47ef5ea15571a9dc4
Dawood Sarfraz <dawoodsarfraz.cs@gmail.com>
2023-06-11 23:14:54 +0500

    Update qr_eigen_values.cpp

--- numerical computing codes/qr_eigen_values.cpp
+++ numerical computing codes/qr_eigen_values.cpp
@@ -3,7 +3,6 @@
  * \brief Compute real eigen values and eigen vectors of a symmetric matrix
  * using [QR decomposition](https://en.wikipedia.org/wiki/QR_decomposition)
  * method.
- * \author [Krishna Vedala](https://github.com/kvedala)
  */
 #include <cassert>
 #include <cmath>
